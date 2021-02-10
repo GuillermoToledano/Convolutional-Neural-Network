76729599352af32afe4aceb2f4b35be54a06ee5d
Guillermo Toledano <GuillermoToledano@users.noreply.github.com>
2021-02-09 23:56:46 -0600

    ReadBatch method now reads the data for each image, creates a tensor and add it to a batch tensor. Added DisplayImage method to create and display an image in a window.

--- CNN/CIFAR.h
+++ CNN/CIFAR.h
@@ -2,21 +2,30 @@
 #ifndef CIFAR_H
 #define CIFAR_H
 
-using namespace std;
+//#include <opennn.h>
+//#include <opencv2/opencv.hpp>
 
 typedef unsigned char uchar;
 
 namespace CIFAR10 {
+
+	void DisplayImage(OpenNN::Tensor<uchar> Image, uchar Label);
+	void ReadBatch(const string path, uint8_t* Labels, OpenNN::Tensor<uchar> &Batch, int BatchSize);
+
 	const size_t Channels = 3;
 	const size_t ImageDim = 32;
-	const size_t BatchSize = 10000;
 	const size_t ImageSize = ImageDim * ImageDim;
 	const size_t ImageBytes = ImageSize * Channels;
-
-	void DisplayImage(OpenNN::Tensor<uchar> Image);
-	void ReadBatch(const string path, uint8_t* Labels, OpenNN::Tensor<uchar> Batch);
-
-	void ReadBatch(const string path, uint8_t *Labels, OpenNN::Tensor<uchar> Batch)
+	const string Strs[] = {"airplane", "automobile", "bird", "cat", "deer", "dog", 
+		                   "frog", "horse", "ship", "truck"};
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="path"></param>
+	/// <param name="Labels"></param>
+	/// <param name="Batch"></param>
+	/// <param name="BatchSize"></param>
+	void ReadBatch(const string path, uint8_t *Labels, OpenNN::Tensor<uchar> &Batch, int BatchSize)
 	{
 		std::ifstream file;
 		file.open(path, std::ios::in | std::ios::binary | std::ios::ate);
@@ -29,71 +38,99 @@ namespace CIFAR10 {
 		std::unique_ptr<char[]> buffer(new char[file_size]);
 		file.seekg(0, std::ios::beg);
 		file.read(buffer.get(), file_size);
-		file.close();
+		file.close();		
 		
-		OpenNN::Matrix<uchar> RChannel(ImageDim, ImageDim, 1);
-		OpenNN::Matrix<uchar> GChannel(ImageDim, ImageDim, 1);
-		OpenNN::Matrix<uchar> BChannel(ImageDim, ImageDim, 1);
-
-		/*OpenNN::Vector<size_t> Dimensions({ImageDim, ImageDim, Channels});
-		OpenNN::Tensor<std::uint8_t> Image(Dimensions, 1);*/
-		OpenNN::Tensor<uchar> Image(Channels, ImageDim, ImageDim);
-		cout << Image.get_dimensions() << endl;
-
-		for (uint16_t i = 0; i < 1; i++)
+		// This loop control the number of images to read
+		for (uint16_t i = 0; i < BatchSize; i++)
 		{
-			Labels[i] = buffer[i * ImageBytes + 1];
+			Labels[i] = buffer[i * (ImageBytes + 1)];
+			OpenNN::Matrix<uchar> RChannel(ImageDim, ImageDim, 0);
+			OpenNN::Matrix<uchar> GChannel(ImageDim, ImageDim, 0);
+			OpenNN::Matrix<uchar> BChannel(ImageDim, ImageDim, 0);
+			OpenNN::Tensor<uchar> Image(ImageDim, ImageDim, Channels);
+			
 			size_t x = 0;
 			size_t y = 0;
+			// This loop reads each image by channel
 			for (uint16_t j = 1; j < ImageSize + 1; j++)
 			{
-				RChannel(x, y) = buffer[i * ImageBytes + j];
-				GChannel(x, y) = buffer[i * ImageBytes + j + 1025];
-				BChannel(x, y) = buffer[i * ImageBytes + j + 2049];
-				// Rows & Cols
-				y++;
-				if (y % ImageDim == 0) {
-					x++;
-					y = 0;
+				// Improve using a vector? for-loop need to use ImageBytes instead
+				// OpenNN::Vector<uchar> Data(ImageBytes);
+				// Data.at(j-1) = buffer[i * ImageBytes + j];
+				RChannel(x, y) = buffer[i * (ImageBytes + 1) + j];
+				GChannel(x, y) = buffer[i * (ImageBytes + 1) + j + 1024];
+				BChannel(x, y) = buffer[i * (ImageBytes + 1) + j + 2048];
+				// Control rows and cols
+				x++;
+				if (ImageDim == x) {
+					y++;
+					x = 0;
 				}
 			}
 			// Fill tensor
 			Image.set_matrix(0, RChannel);
 			Image.set_matrix(1, GChannel);
 			Image.set_matrix(2, BChannel);
-			// 
-			//Batch.set_tensor(i, Image);
-			//DisplayImage(Image);
+			// If using vector, look for embed method in Tensor class
+			// Image.embed(0, Data);
+			// Add Image tensor to Batch tensor
+			Batch.set_tensor(i, Image);
 		}		
 	}
-
-	void DisplayImage(OpenNN::Tensor<uchar> Image) {
-		uchar Data[3072];
-		uchar Dim = 32;
-		const size_t rank = Image.get_dimension(2);
-		const size_t rows = Image.get_dimension(0);
-		const size_t cols = Image.get_dimension(1);
-
-		size_t x = 0;
-		for (size_t k = 0; k < rank; k++)
+	/// <summary>
+	/// Creates a window and display an image.
+	/// </summary>
+	/// <param name="Image">An tensor that conteins the image's data with dimensions (32, 32, 3)</param>
+	/// <param name="Label">A value that represent the image's class</param>
+	void DisplayImage(OpenNN::Tensor<uchar> Image, uchar Label)
+	{
+		uchar Data[3072]{};
+		for (size_t i = 0; i < ImageSize; i++)
 		{
-			for (size_t i = 0; i < rows; i++)
+			for (size_t j = 0; j < Channels; j++)
 			{
-				for (size_t j = 0; j < cols; j++)
-				{
-					Data[x] = Image(i, j, k);
-					x++;
-				}
+				Data[i * Channels + j] = Image[j * ImageSize + i];
 			}
-		}	
-		
-		cv::Mat Img(ImageDim, ImageDim, CV_8UC3, Data);
-		cv::imshow("CIFAR", Img);
-		char key = cv::waitKey(0);
-		if (key == 's')
-		{
-			imwrite("test.png", Img);
 		}
+		cv::Mat Img(ImageDim, ImageDim, CV_8UC3, Data, ImageDim * Channels);
+		cv::cvtColor(Img, Img, cv::COLOR_RGB2BGR, Channels);
+		switch (Label)
+		{
+		case 0:
+			cout << Strs[0] << endl;
+			break;
+		case 1:
+			cout << Strs[1] << endl;
+			break;
+		case 2:
+			cout << Strs[2] << endl;
+			break;
+		case 3:
+			cout << Strs[3] << endl;
+			break;
+		case 4:
+			cout << Strs[4] << endl;
+			break;
+		case 5:
+			cout << Strs[5] << endl;
+			break;
+		case 6:
+			cout << Strs[6] << endl;
+			break;
+		case 7:
+			cout << Strs[7] << endl;
+			break;
+		case 8:
+			cout << Strs[8] << endl;
+			break;
+		case 9:
+			cout << Strs[9] << endl;
+			break;
+		}
+		cv::namedWindow("CIFAR-10 Sample", cv::WINDOW_FREERATIO | cv::WINDOW_GUI_EXPANDED);
+		cv::imshow("CIFAR-10 Sample", Img);
+		cv::waitKey(0);
+		cv::destroyWindow("CIFAR-10 Sample");
 	}
 }
 
--- CNN/CNN.cpp
+++ CNN/CNN.cpp
@@ -6,7 +6,7 @@
 #include <opencv2/opencv.hpp>
 #include "CIFAR.h"
 
-using namespace cv;
+//using namespace cv;
 using namespace std;
 using namespace OpenNN;
 using namespace CIFAR10;
@@ -15,12 +15,53 @@ typedef unsigned char uchar;
 
 int main()
 {
+    // Batch size
+    const int Images = 20;
+    // Strings
     string Path = "../batches-bin/data_batch_1.bin";
 
-    uint8_t Labels[1];
-    OpenNN::Tensor<uchar> Batch;
-    CIFAR10::ReadBatch(Path, Labels, Batch);
+    char answer;
+    // Store all image labels
+    uint8_t Labels[Images];
+    /* 
+     Images:    Total of images to store
+     ImageDim:  First dimension of the stored tensors
+     ImageDim:  Second dimension of the stored tensors
+     Channels:  Third dimension of the stored tensors
+    */
+    OpenNN::Tensor<uchar> Batch(Images, CIFAR10::ImageDim, CIFAR10::ImageDim, CIFAR10::Channels);
 
+    CIFAR10::ReadBatch(Path, Labels, Batch, Images);
+    cout << "Display images? (y/n) ";
+    cin >> answer;
+    cout << endl;
+    if (answer == 'y')
+    {
+        for (size_t i = 0; i < Images; i++)
+        {
+            OpenNN::Tensor<uchar> Image = Batch.get_tensor(i);
+            CIFAR10::DisplayImage(Image, Labels[i]);
+            Image.~Tensor();
+        }
+    }    
+    
+    const Vector<size_t> Dimensions({3, 32, 32});
+    const Vector<size_t> Filter({32, 5, 5});
+    
+    // CifarNet architecture
+    OpenNN::NeuralNetwork CNN;
+    /*
+    // Convolutional Layer 1
+    OpenNN::ConvolutionalLayer* ConvLayer_1 = new OpenNN::ConvolutionalLayer(Dimensions, Filter);
+    CNN.add_layer(ConvLayer_1);
+    const Vector<size_t> CL1_OutputDims = ConvLayer_1->get_outputs_dimensions();
 
+    // Pooling Layer 1
+    OpenNN::PoolingLayer* PoolLayer_1 = new OpenNN::PoolingLayer(CL1_OutputDims);
+    CNN.add_layer(PoolLayer_1);
+    const Vector<size_t> PL1_OutputDims = PoolLayer_1->get_outputs_dimensions();
+    
+    CNN.print_summary();
+    */
     return 0;
 }
